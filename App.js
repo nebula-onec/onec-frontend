038c4659e0855b295c4f72cd8fa22790fbecb9a2
Abc <abc@example.com>
2022-12-13 17:58:33 +0530

    second commit

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import  { createNativeStackNavigator } from '@react-navigation/native-stack';
 export default function App() {
   return (
     <NavigationContainer>
-      
+      <Screen></Screen>
     </NavigationContainer>
   );
 }
