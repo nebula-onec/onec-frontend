cda9b3b270d953be717cf815b095df52a651ea82
Mit2561 <patelm1925@gmail.com>
2022-12-24 23:38:02 +0530

    changes in app.js

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { AppState, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import  { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -13,8 +12,23 @@ export default function App() {
   const Stack = createNativeStackNavigator();
 
   return (
-    <NavigationContainer>
-      <Screen></Screen>
+      <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen 
+          name="Splash" 
+          component={SplashScreen}
+          options={{
+            headerShown: false,
+          }}
+         />
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen}
+          options={{
+            headerShown: false,
+          }}
+         />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
