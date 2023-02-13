fd8b15b33c18da2dc78b0958ae6aa9f4042fb620
Surya <adarshrawat.run@gmail.com>
2023-02-13 17:27:56 +0530

    reinserted value.js

---
+++ assets/config/values.js
@@ -0,0 +1,13 @@
+export default{
+    primary: '#4d90fe',
+    primary_background: '#fff',
+    white: '#fff',
+    black: '#000',
+    red:"red",
+
+    //for font sizes,
+    fontSmall: 16,
+    fontMid: 18,
+    fontLarge: 24,
+    fontxSmall: 14,
+}
